6ea3689a116d380e132c54591e33ace448b44661
lam <lam@scolopendre.informatique.univ-paris-diderot.fr>
2016-10-03 16:50:46 +0200

    Question 3

--- src/TP1/ex1_UDP/ServeurUDP.java
+++ src/TP1/ex1_UDP/ServeurUDP.java
@@ -3,12 +3,13 @@ package TP1.ex1_UDP;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 /**
- * 1. Si ce serveur est utilisé par plusieurs clients C1 et C2, C1 envoyant un
- * long message, alors les lettres de C2 remplaceront la première partie de C1,
- * sans troncaturer le message. Le byte[] receiveData doit être vidé.
+ * 1. Si ce serveur est utilisï¿½ par plusieurs clients C1 et C2, C1 envoyant un
+ * long message, alors les lettres de C2 remplaceront la premiï¿½re partie de C1,
+ * sans troncaturer le message. Le byte[] receiveData doit ï¿½tre vidï¿½.
  * 
  */
 public class ServeurUDP {
@@ -19,17 +20,23 @@ public class ServeurUDP {
 			while (true) {
 				//@TODO Question 3
 				byte[] receiveData = new byte[1024];
+				byte[] sendData = new byte[1024];
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				serverSocket.receive(receivePacket);
+				InetAddress UserAddress = receivePacket.getAddress(); 
+				int UserPort = receivePacket.getPort(); 
 				String sentence = new String(receivePacket.getData());
 				System.out.println(
-						serverSocket.getLocalAddress() + ", " + serverSocket.getLocalPort() + " : " + sentence);
+						UserAddress + ", " + UserPort + " : " + sentence);
+				sendData = "ReÃ§u du Serveur: MessageAuClient".getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, UserAddress, UserPort);
+				serverSocket.send(sendPacket);
 			}
 		} catch (SocketException e) {
-			System.err.println("Erreur de création ou d'accès à une socket, port " + port);
+			System.err.println("Erreur de crï¿½ation ou d'accï¿½s ï¿½ une socket, port " + port);
 			e.printStackTrace();
 		} catch (IOException e) {
-			System.err.println("Echec de réception d'un DatagramPacket");
+			System.err.println("Echec de rï¿½ception d'un DatagramPacket");
 			e.printStackTrace();
 		}
 	}
