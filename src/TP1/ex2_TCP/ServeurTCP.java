cbe74aec475454612c18ab3a73490290d61599c2
lam <lam@scolopendre.informatique.univ-paris-diderot.fr>
2016-10-03 18:32:38 +0200

    Question 6

--- src/TP1/ex2_TCP/ServeurTCP.java
+++ src/TP1/ex2_TCP/ServeurTCP.java
@@ -1,10 +1,71 @@
 package TP1.ex2_TCP;
 
-public class ServeurTCP {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ServeurTCP {
+	static int nombreClients = 0;
+	
+	class Messagerie extends Thread{
+		Socket socketClient;
+		
+		public Messagerie(Socket socketClient){
+			this.socketClient = socketClient;
+		}
+		
+		public void run(){
+				try {
+					PrintWriter pw = new PrintWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+					while(true){
+						pw.write("Nombre de clients" + nombreClients);
+						Thread.sleep(10000);
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					nombreClients--;
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					nombreClients--;
+					e.printStackTrace();
+				}
+			}
+	}
+	
+	public void demarrer(){
+		try(ServerSocket server = new ServerSocket(1027)){
+			while(true){
+				Socket socket = server.accept();
+				nombreClients++;
+				InetAddress UserAddress = socket.getInetAddress();
+				int UserPort = socket.getPort();
+				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+				String message = br.readLine();
+				System.out.println("(UserAddress/Port) " + UserAddress + "/" + UserPort + " : " + message );
+				pw.write("Bienvenue !");
+				pw.flush();
+				Messagerie m = new Messagerie(socket);
+				m.start();
+				br.close();
+				pw.close();
+			}
+			
+		}catch(Exception e){
+			System.out.println(e);
+			e.printStackTrace();
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ServeurTCP server = new ServeurTCP();
+		server.demarrer();
 	}
 
 }
