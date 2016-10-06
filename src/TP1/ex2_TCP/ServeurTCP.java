8f561300d830531664e046d35f16d3dafa4e168b
KhauKevin <khau@Absolute>
2016-10-06 03:17:26 +0200

    TODO à revoir + Message simultané basé le serveur
    Le message [Nombre de clients] est envoyé simultanément à tous les
    clients
    Les clients ont désormais un identifiant (entier) pseudo-unique attribué
    par le serveur
    Important et pas encore géré : repérer et alerter de la déconnexion d'un
    client, ligne 8 du protocole encore à implémenter, donc
    +Voir les TODOs : Corriger, ou commenter // LAM pour suggestion

--- src/TP1/ex2_TCP/ServeurTCP.java
+++ src/TP1/ex2_TCP/ServeurTCP.java
@@ -8,55 +8,123 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
 
+// TODO Repérer quand une connexion est fermée, et retirer les clients correspondants (par exemple quand arrêt d'un programme ClientTCP)
 public class ServeurTCP {
+	// TODO Déplacer nombreClients dans le Manager ?
 	static int nombreClients = 0;
-	int port = 1027;
+	final int connectionPort = 1027;
+	Manager manager = new Manager();
 
-	class Messagerie extends Thread {
-		Socket socketClient;
+	/**
+	 * Envoie des messages à un client spécifique
+	 */
+	class Messagerie {
+		Socket socket;
 		PrintWriter pw;
 
-		public Messagerie(Socket socketClient, PrintWriter pw) {
-			this.socketClient = socketClient;
+		public Messagerie(Socket socket, PrintWriter pw) {
+			this.socket = socket;
 			this.pw = pw;
 		}
 
-		public void run() {
-			try {
-				while (true) {
-					pw.write("Nombre de clients: " + nombreClients + "\n");
-					pw.flush();
-					Thread.sleep(10000);
-				}
-			} catch (InterruptedException e) {
-				nombreClients--;
-				e.printStackTrace();
-			}
+		public void welcome(int id) {
+			pw.write("Bienvenue ! Vous êtes le client " + id + ".\n");
+			pw.flush();
+		}
+
+		public void send() {
+			pw.write("Nombre de clients: " + nombreClients + ".\n");
+			pw.flush();
+		}
+
+		public void alertNewcomer(int id) {
+			pw.write("Un nouvel ami est arrivé : 'Client " + id + "' (" + socket.getInetAddress() + "/"
+					+ socket.getPort() + ")\n");
+			pw.flush();
 		}
 	}
 
-	public void demarrer() {
-		try (ServerSocket server = new ServerSocket(port)) {
-			
+	/**
+	 * Synchronize les messages pour des communications simultanées
+	 */
+	class Manager extends Thread {
+		final int sleepTimeMs = 10000;
+		int count = 1; // Pour l'ID d'un client, pas obligatoirement en relation
+						// avec le nombre total de clients
+		private LinkedList<Messagerie> messageries = new LinkedList<>();
+
+		/**
+		 * Appelle la méthode .send() de chaque messagerie toutes les 10s
+		 */
+		public void run() {
 			while (true) {
+				try {
+					// TODO ConcurrentModificationException aléatoire à corriger
+					for (Messagerie m : messageries) {
+						m.send();
+					}
+					Thread.sleep(sleepTimeMs);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public synchronized void addMessenger(Messagerie m) {
+			messageries.add(m);
+			// TODO ID attribué par count pas totalement unique, parce qu'on
+			// revient sur le même nombre après Integer.MAX*2
+			m.welcome(count);
+			for (Messagerie messagerie : messageries) {
+				messagerie.alertNewcomer(count);
+			}
+			count++;
+			if (messageries.size() == 1) {
+				start();
+			}
+		}
+
+		// TODO Trouver quand appeler cette méthode.
+		public synchronized void removeMessenger(Messagerie m) {
+			messageries.remove(m);
+			if (messageries.isEmpty()) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	// TODO Vérifier que partout, nombreClients == manager.messageries.size() ==
+	// [Print Writers ouverts]
+	public void demarrer() {
+		try (ServerSocket server = new ServerSocket(connectionPort)) {
+			System.out.println("Lancement serveur : port " + connectionPort + ".");
+			while (true) {
+
 				Socket socket = server.accept();
 				nombreClients++;
 				InetAddress userAddress = socket.getInetAddress();
 				int userPort = socket.getPort();
+
+				// TODO Vérifier scrupuleusement s'ils sont fermés
+				// convenablement
 				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 				String message = br.readLine();
 				System.out.println("(UserAddress/Port) " + userAddress + "/" + userPort + " : " + message);
-				pw.write("Bienvenue !\n");
-				pw.flush();
-				Messagerie m = new Messagerie(socket, pw);
-				m.start();
+				manager.addMessenger(new Messagerie(socket, pw));
+
 			}
 		} catch (IllegalArgumentException e) {
 			System.err.println("Valeur de port invalide, doit être entre 0 et 65535");
 		} catch (IOException e) {
-			System.err.println("Problème de traitement de la socket : port " + port);
+			System.err.println("Problème de traitement de la socket : port " + connectionPort);
 			e.printStackTrace();
 		}
 	}
